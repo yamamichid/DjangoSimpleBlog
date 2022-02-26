e037e60002107dfab5462c89576f0fb4c8ac57c4
etmula <mountainroad@hotmail.co.jp>
2022-02-26 21:30:48 +0900

    Add Comment model

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.article.title}-{self.created_at.date()}-{self.author}'
